2703e34eef3f8b961d9ddc5abb1112ad1da5d2ea
JustinShih0918 <s0910283@my.cmsh.cyc.edu.tw>
2024-09-20 02:00:08 +0800

    use always version, pass

--- Lab2_basic_practice/lab2_2.v
+++ Lab2_basic_practice/lab2_2.v
@@ -3,13 +3,22 @@ module lab2_2(
     input wire rst,
     output reg [15:0] out// You can modify "reg" to "wire" if needed
 );
-    wire out_next = (out % 2 != 0) ? out * 2 : out + 1; 
+    reg [15:0] round;
     always @(posedge clk) begin
+        $display("out0=%d, round=%d", out[0], round);
         if(rst) begin
             out <= 16'B1;
+            round <= 1;
         end
         else begin
-            out <= out_next;
+            if(out[0] == 1'B1) begin
+                out = out * 2;
+                round = round + 1;
+            end
+            else begin
+                out <= out + round;
+                round <= round + 1;
+            end
         end
     end
 
