7b9eccdad4c19d32867398c74d5000349cc514fe
JustinShih0918 <s0910283@my.cmsh.cyc.edu.tw>
2024-09-26 02:47:28 +0800

    correct version

--- Lab2_adv/lab2_adv_1.v
+++ Lab2_adv/lab2_adv_1.v
@@ -11,11 +11,13 @@ module lab2_adv_1 (
     reg [3:0] h;
     reg [11:0] code_de;
     always @* begin
-        h[0] = code[0] ^ code[2] ^ code[4] ^ code[6] ^ code[8] ^ code[10];
-        h[1] = code[1] ^ code[2] ^ code[5] ^ code[6] ^ code[9] ^ code[10];
-        h[2] = code[3] ^ code[4] ^ code[5] ^ code[6] ^ code[11];
-        h[3] = code[7] ^ code[8] ^ code[9] ^ code[10] ^ code[11];
+        $display("code: %b", code);
+        h[0] = code[11] ^ code[9] ^ code[7] ^ code[5] ^ code[3] ^ code[1];
+        h[1] = code[10] ^ code[9] ^ code[6] ^ code[5] ^ code[2] ^ code[1];
+        h[2] = code[8] ^ code[7] ^ code[6] ^ code[5] ^ code[0];
+        h[3] = code[4] ^ code[3] ^ code[2] ^ code[1] ^ code[0];
         code_de = code;
+        $display("code_de: %b", code_de);
     end
 
     always @(posedge clk, negedge rst_n) begin
@@ -27,22 +29,25 @@ module lab2_adv_1 (
         end
         else if(h > 12) begin //multiple errors
             out = 0;
-            raw_data = 0;
+            raw_data = 8'b0;
             err = 1;
             cor = 0;
+            $display("Multiple errors, %d", h);
         end
-        else if(h == 0) begin //no error
-            out = 0;
-            raw_data = {code_de[2], code_de[4], code_de[5], code_de[6], code_de[8], code_de[9], code_de[10], code_de[11]};
-            err = 0;
-            cor = 1;
-        end
-        else begin //one error
+        else if(h <=12 && h >=1) begin //one error
             out = h;
-            code_de[h-1] = ~code[h-1];
-            raw_data = {code_de[2], code_de[4], code_de[5], code_de[6], code_de[8], code_de[9], code_de[10], code_de[11]};
+            code_de[12 - h] = ~code[12 - h];
+            raw_data = {code_de[9], code_de[7], code_de[6], code_de[5], code_de[3], code_de[2], code_de[1], code_de[0]};
             err = 0;
             cor = 0;
+            $display("one error, %d", h);
+        end
+        else begin //no error
+            out = h;
+            raw_data = {code_de[9], code_de[7], code_de[6], code_de[5], code_de[3], code_de[2], code_de[1], code_de[0]};
+            err = 0;
+            cor = 1;
+            $display("no error, %d", h);
         end
     end
 // Output signals can be reg or wire
