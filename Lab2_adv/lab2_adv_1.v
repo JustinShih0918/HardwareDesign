9b7e3bd23ab7dc36b9f5ccad36535032d4fb3e9e
JustinShih0918 <72808967+JustinShih0918@users.noreply.github.com>
2024-09-25 23:20:32 +0800

    design finish

--- Lab2_adv/lab2_adv_1.v
+++ Lab2_adv/lab2_adv_1.v
@@ -2,13 +2,47 @@
 module lab2_adv_1 (
     input clk,
     input rst_n, 
-    input [11:0] code, 
+    input reg [11:0] code, 
     output reg [3:0] out,
     output reg [7:0] raw_data,
     output reg err,
     output reg cor
 );
+    wire [3:0] h;
+    always @* begin
+        h[0] = code[0] ^ code[2] ^ code[4] ^ code[6] ^ code[8] ^ code[10];
+        h[1] = code[1] ^ code[2] ^ code[5] ^ code[6] ^ code[9] ^ code[10];
+        h[2] = code[3] ^ code[4] ^ code[5] ^ code[6] ^ code[11];
+        h[3] = code[7] ^ code[8] ^ code[9] ^ code[10] ^ code[11];
+    end
 
+    always @(posedge clk, negedge rst_n) begin
+        if(rst_n == 1'b0) begin //reset
+            out = 4'b0;
+            raw_data = 8'b0;
+            err = 1'b0;
+            cor = 1'b0;
+        end
+        else if(h > 12) begin //multiple errors
+            out = 0;
+            raw_data = 0;
+            err = 1;
+            cor = 0;
+        end
+        else if(h == 0) begin //no error
+            out = 0;
+            raw_data = {code[2], code[4], code[5], code[6], code[8], code[9], code[10], code[11]};
+            err = 0;
+            cor = 1;
+        end
+        else begin //one error
+            out = h;
+            code[h-1] = ~code[h-1];
+            raw_data = {code[2], code[4], code[5], code[6], code[8], code[9], code[10], code[11]};
+            err = 0;
+            cor = 0;
+        end
+    end
 // Output signals can be reg or wire
 // add your design here
    
--- Lab2_adv/lab2_adv_1_t.v
+++ Lab2_adv/lab2_adv_1_t.v
@@ -33,18 +33,34 @@ module lab2_adv_1_t ();
     //101100110011
     //010010011101
     //111111011011
-
+    reg [11:0] test_case [7:0];
+    initial begin
+        test_case[0] = 12'b000000011001;
+        test_case[1] = 12'b101110100111;
+        test_case[2] = 12'b110010111111;
+        test_case[3] = 12'b101111101010;
+        test_case[4] = 12'b110100100001;
+        test_case[5] = 12'b101100110011;
+        test_case[6] = 12'b010010011101;
+        test_case[7] = 12'b111111011011;
+    end
+    
 
     // TODO 2
     // Connect your lab2_adv_1 module here 
     // Please connect it by port name but not order
-    lab2_adv_1 mou1();
+    lab2_adv_1 mou1(.clk(clk), .rst_n(rst_n), .code(code), .out(out), .raw_data(raw_data), .err(err), .cor(cor));
     
 
     // TODO 3
     //write some code here to change the input of the moudle above
+    integer i;
     initial begin
-        
+        $DELAY(35);
+        for(i = 0; i < 8; i = i + 1) begin
+            code = test_case[i];
+            #10;
+        end
     end
     //====================================
     
