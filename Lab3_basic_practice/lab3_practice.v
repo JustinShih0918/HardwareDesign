f4d155367432ce726ece3d470d78eddbb414cf1c
JustinShih0918 <s0910283@my.cmsh.cyc.edu.tw>
2024-10-04 03:53:51 +0800

    sucessful version

--- Lab3_basic_practice/lab3_practice.v
+++ Lab3_basic_practice/lab3_practice.v
@@ -8,19 +8,20 @@ module lab3_practice (
 ); 
     wire clk_fast;
     wire clk_slow;
-    reg [15:0] led_slow;
-    reg [15:0] led_fast;
+    reg rst_led;
+    wire [15:0] led_slow;
+    wire [15:0] led_fast;
+    reg [1:0] state, next_state;
     clock_divider #(.n(27)) fast_clk_divider(.clk(clk), .clk_div(clk_fast));
-    clock_divider #(.n(28)) slow_clk_divider(.clk(clk_fast), .clk_div(clk_slow));
-    LED slow_led(.clk(clk_slow), .led(led_slow));
-    LED fast_led(.clk(clk_fast), .led(led_fast));
+    clock_divider #(.n(28)) slow_clk_divider(.clk(clk), .clk_div(clk_slow));
+    LED slow_led(.clk(clk_slow), .state(state), .next_state(next_state), .led(led_slow));
+    LED fast_led(.clk(clk_fast), .state(state), .next_state(next_state), .led(led_fast));
 
     parameter INITIAL = 0;
     parameter SLOW = 1;
     parameter FAST = 2;
 
     // FSM
-    reg [1:0] state, next_state;
     always @(posedge clk, posedge rst) begin
         if(rst) begin
             state = INITIAL;
@@ -35,21 +36,35 @@ module lab3_practice (
         case (state)
             INITIAL: begin
                 next_state = SLOW;
+                rst_led = 1;
                 led = 16'b1111111111111111;
             end 
             SLOW: begin
-                if(speed) next_state = FAST;
-                else next_state = SLOW;
+                if(speed) begin
+                    next_state = FAST;
+                    rst_led = 1;
+                end
+                else begin
+                    next_state = SLOW;
+                    rst_led = 0;
+                end
                 led = led_slow;
             end
             FAST: begin
-                if(!speed) next_state = SLOW;
-                else next_state = FAST;
+                if(!speed) begin
+                    next_state = SLOW;
+                    rst_led = 1;
+                end
+                else begin
+                    next_state = FAST;
+                    rst_led = 0;
+                end
                 led = led_fast;
             end
-            default: 
+            default: begin
                 next_state = INITIAL;
-                led = 16'b1111111111111111;
+                led = 16'b1111111111111111; 
+            end
         endcase
     end
 
@@ -75,25 +90,29 @@ endmodule
 
 module LED(
     input wire clk,
-    output reg [15:0] led;
+    input wire state,
+    input wire next_state,
+    output reg [15:0] led
 );
 
-    integer now_idx = 0;
-    integer pre_idx = 0;
-    integer i;
-    always @(posedge clk) begin
-
-        //reset
-        for(i = 0; i < 16; i = i + 1) begin
-            led[i] = 0;
+    reg [3:0] now_idx;
+    reg [3:0] pre_idx;
+    wire rst;
+    assign rst = (state != next_state);
+    always @(posedge clk, posedge rst) begin
+        if(rst) begin
+            pre_idx = 0;
+            now_idx = 0;
+            led = 16'b0000000000000001;
         end
-
-        led[pre_idx] = 0;
-        led[now_idx] = 1;
-
-        pre_idx = now_idx;
-        if(now_idx == 15) now_idx = 0;
-        else now_idx = now_idx + 1;
+        else begin
+            pre_idx = now_idx;
+            if(now_idx == 15) now_idx = 0;
+            else now_idx = now_idx + 1;
+            
+            led[pre_idx] = 0;
+            led[now_idx] = 1;
+        end
     end
 
 endmodule
