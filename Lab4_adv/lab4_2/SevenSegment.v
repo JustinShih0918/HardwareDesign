24eb9337b742d806a149dcfb9f37fe7ca659f4a4
JustinShih0918 <72808967+JustinShih0918@users.noreply.github.com>
2024-10-27 19:49:47 +0800

    right file

--- Lab4_adv/lab4_2/SevenSegment.v
+++ Lab4_adv/lab4_2/SevenSegment.v
@@ -49,16 +49,21 @@ module SevenSegment(
     
     always @ (*) begin
     	case (display_num)
-    		0 : display = 7'b1000000;	//0000
+    		0 : display = 7'b1000000;	//0000 / O
 			1 : display = 7'b1111001;   //0001                                                
 			2 : display = 7'b0100100;   //0010                                                
 			3 : display = 7'b0110000;   //0011                                             
 			4 : display = 7'b0011001;   //0100                                               
-			5 : display = 7'b0010010;   //0101                                               
+			5 : display = 7'b0010010;   //0101 / S                                           
 			6 : display = 7'b0000010;   //0110
 			7 : display = 7'b1111000;   //0111
 			8 : display = 7'b0000000;   //1000
 			9 : display = 7'b0010000;	//1001
+			10: display = 7'b1100010;   // W
+			11: display = 7'b1001111;   // I
+			12: display = 7'b1001000;   // N
+			13: display = 7'b1000111;   // L
+			14: display = 7'b0000110;   // E
 			4'b1111 : display = 7'b0111111;
 			default : display = 7'b1111111;
     	endcase
