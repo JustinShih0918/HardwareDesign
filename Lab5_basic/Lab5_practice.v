83dd8f7f9701cb9b13384ceb63c4a99545f6492e
JustinShih0918 <72808967+JustinShih0918@users.noreply.github.com>
2024-11-04 16:04:38 +0800

    lab practice start

---
+++ Lab5_basic/Lab5_practice.v
@@ -0,0 +1,37 @@
+module lab5_practice (
+    input wire clk,
+    input wire rst,
+    input wire [2:0] addr,
+    input wire we, 
+    input wire [7:0] din,
+    input wire re,
+    input wire start,
+    output reg [7:0] dout,
+    output reg done,
+    output reg [7:0] ans
+);
+    // add your design here
+    // note that you are free to adjust the IO's data type
+    reg [7:0] memory [0:5];
+    integer i;
+    always @(posedge clk) begin
+        if(rst) begin
+            for(i = 0 ;i < 6; i = i + 1) memory[i] <= 8'b0;  
+            dout <= 0;
+        end
+        else if(we) memory[addr] <= din;
+        else if(re) dout <= memory[addr];
+    end
+
+    always @(posedge clk) begin
+        if(rst) begin
+            ans <= 0;
+            done <= 0;
+        end
+        else i(start) begin
+            for(i = 0; i < 6; i = i + 1) ans <= ans + memory[i];
+            done <= 1;
+        end
+    end
+    
+endmodule
