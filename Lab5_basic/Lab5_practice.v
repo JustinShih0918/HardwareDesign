d7b113d254528642560a77eb36f93fba2b3f1bd3
JustinShih0918 <72808967+JustinShih0918@users.noreply.github.com>
2024-11-04 17:09:53 +0800

    use one block

--- Lab5_basic/Lab5_practice.v
+++ Lab5_basic/Lab5_practice.v
@@ -13,38 +13,29 @@ module lab5_practice (
     // add your design here
     // note that you are free to adjust the IO's data type
     reg [7:0] memory [0:5];
-    reg en_add;
-    reg finish_add;
+    reg [2:0] idx;
+    reg [7:0] sum;
     integer i;
     always @(posedge clk) begin
         if(rst) begin
             for(i = 0 ;i < 6; i = i + 1) memory[i] <= 8'b0;  
             dout <= 0;
+            ans <= 0;
+            sum <= 0;
+            idx <= 0;
+            done <= 0;
         end
         else if(we) memory[addr] <= din;
         else if(re) dout <= memory[addr];
-        else if(start) en_add <= 1;
-        else if(done) en_add <= 0;
-    end
-
-    reg [2:0] idx;
-    reg [7:0] sum;
-    always @(posedge clk) begin
-        if(rst) begin
-            ans <= 0;
-            done <= 0;
-            idx <= 0;
-            sum <= 0;
-        end
-        else if(en_add && !done) begin
-            sum <= sum + memory[idx];
-            idx <= idx + 1;
-            if(idx == 5) begin
-                done <= 1;
-                idx <= 0;
+        else if(start) begin
+            if(idx < 6) begin
+                sum <= sum + memory[idx];
+                idx <= idx + 1;
+            end
+            else begin
                 ans <= sum;
+                done <= 1;
             end
         end
     end
-    
 endmodule
