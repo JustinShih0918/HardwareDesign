5b115c3b6805abfd760199a52f09f1b84cd8e05d
JustinShih0918 <s0910283@my.cmsh.cyc.edu.tw>
2024-11-16 15:07:25 +0800

    zoom design

--- Lab5_adv/SampleCode_VGA/demo2/mem_addr_gen.v
+++ Lab5_adv/SampleCode_VGA/demo2/mem_addr_gen.v
@@ -6,17 +6,32 @@ module mem_addr_gen(
    output [16:0] pixel_addr
    );
     
-   reg [7:0] position;
+   reg [9:0] pos_x;
+   reg [9:0] pos_y; 
+   parameter IMG_W = 320;
+   parameter IMG_H = 240;
+
+   wire [9:0] center_x = IMG_W/2;
+   wire [9:0] center_y = IMG_H/2;
+
+   parameter ZOOM = 2;
+
+   wire [9:0] xpos = center_x + ((h_cnt - 320) >> ZOOM);
+   wire [9:0] ypos = center_y + ((v_cnt - 240) >> ZOOM);
   
-   assign pixel_addr = ((h_cnt>>1)+320*(v_cnt>>1)+ position*320 )% 76800;  //640*480 --> 320*240 
+   assign pixel_addr = ((xpos % IMG_W) + IMG_W * (ypos % IMG_H)) % 76800;  //640*480 --> 320*240 
 
    always @ (posedge clk or posedge rst) begin
-      if(rst)
-          position <= 0;
-       else if(position < 239)
-           position <= position + 1;
-       else
-           position <= 0;
+      if(rst) begin
+        pos_x <= 0;
+          pos_y <= 0;
+      end                                                                                                                
+       else begin
+          pos_x <= pos_x + 1;
+          pos_y <= pos_y + 1;
+          if(pos_x == 319) pos_x <= 0;
+          else if(pos_y == 239) pos_y <= 0;
+       end
    end
     
 endmodule
