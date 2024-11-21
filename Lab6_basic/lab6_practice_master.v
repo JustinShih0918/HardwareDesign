660c600dc24ba795162e37a6d1f4392e48e36424
JustinShih0918 <72808967+JustinShih0918@users.noreply.github.com>
2024-11-21 15:28:59 +0800

    lab6 practice

---
+++ Lab6_basic/lab6_practice_master.v
@@ -0,0 +1,10 @@
+module lab6_practice_master (
+	input wire clk,
+	input wire rst,
+	input wire [7:0] sw, // switches
+	output wire [3:0] data_out  // data (number) to slave 
+);
+
+// add your design here
+endmodule
+
